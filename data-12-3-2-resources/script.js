18dfb6431caaedd90f5fd2d76fe780d5e38a3b28
kflores4716 <kflores4716@gmail.com>
2022-03-10 12:11:13 -0700

    adding github pages sample

---
+++ data-12-3-2-resources/script.js
@@ -0,0 +1,22 @@
+// Retrieve data from external data file
+
+// d3.json("samples.json").then(function(data){
+//     console.log(data);
+// });
+
+// d3.json("samples.json").then(function(data){
+//     firstPerson = data.metadata[0];
+//     Object.entries(firstPerson).forEach(([key, value]) =>
+//       {console.log(key + ': ' + value);});
+// });
+
+d3.selectAll("body").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+};
